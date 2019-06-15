bc8ed45e681cd8b23ca6985bf7f7cf56e3d682b7
fttlanshang <fttlanshang@gmail.com>
2019-06-15 19:38:50 +0800

    create a new project

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\20190614_python back end\tango_with_django_project\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
