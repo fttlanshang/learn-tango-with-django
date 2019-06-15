c988727db2bb2d8bd09385d8bf2764dd173180da
fttlanshang <fttlanshang@gmail.com>
2019-06-15 21:50:45 +0800

    finish chapter 4

--- rango/views.py
+++ rango/views.py
@@ -3,10 +3,9 @@ from django.http import HttpResponse
 
 
 def index(request):
-    html = "Hello rango, bro! Welcome page <a href='/rango/about'> ABOUT </a>"
-    return HttpResponse(html)
+    context_dict = {"boldmessage": "A lovely dog!"}
+    return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-    html = "This is the about page. Come back to <a href='/rango/'> Index </a>"
-    return HttpResponse(html)
+    return render(request, 'rango/about.html', context=None)
