8581dc79630e487d52cb88c94eeb95a03050f345
fttlanshang <fttlanshang@gmail.com>
2019-06-16 12:15:56 +0800

    finish chapter 5

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from rango.models import Category
+
 
 # Create your tests here.
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        """
+        ensure the views of a category is equal to or above 0
+        """
+        cat = Category(name='test', views=1, likes=0)
+        cat.save()
+        self.assertEqual((cat.views >= 0), True)
+
+    # def test_slug_line_creation(self):
+    #     cat = Category(name="Random slug creation test")
+    #     cat.save()
+    #     self.assertEqual(cat.slug, 'random-slug-creation-test')
